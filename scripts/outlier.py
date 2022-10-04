7345425c2bd901152d73ffa8ee0c0585fe4db6fe
KaurArshnoor <arshnoorstudent@gmail.com>
2022-10-04 13:43:57 +0000

    outlier work combined

--- scripts/outlier.py
+++ scripts/outlier.py
@@ -16,40 +16,8 @@ spark = (
 )
 
 #============================================================================================
-### INTERNAL DATASET
+### REMOVE OUTLIERS as detected in outlier analysis notebook
 #--------------------------------------------------------------------------------------------
-internal = ETL.final_join
-# print(internal.head(1))
-# internal.printSchema()
-
-print("Count before outliers: ", internal.count())
-# Excluding transactions with no merchants
-internal1 = internal.filter("merchant_abn IS NOT NULL")
-print("Count after outlier exclusion 1: ", internal1.count())
-
-# # Excluding transactions with $0
-# internal2 = internal1.filter(internal1.dollar_value > 0)
-# print("Count after outlier exclusion 2: ", internal2.count())
-
-# # Excluding merchants with no transactions and record merchant name
-# merchants_no_trans = internal2.filter("consumer_id IS NULL")
-# internal3 = internal2.filter("consumer_id IS NOT NULL")
-# print("Count after outlier exclusion 3: ", internal3.count())
-
-#--------------------------------------------------------------------------------------------
-selected_columns = internal1.select("merchant_abn","dollar_value")
-aggregated_revenue = selected_columns.groupby("merchant_abn").sum("dollar_value")
-aggregated_revenue_pd = aggregated_revenue.toPandas()
-
-#--------------------------------------------------------------------------------------------
-
-
-
-
-
-
-
-
-
+final_dataset = ETL.final_join3.filter("merchant_abn IS NOT NULL")
 
 #--------------------------------------------------------------------------------------------
